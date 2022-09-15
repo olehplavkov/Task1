533c23f1daa318620de8f3169781c74d94ae818a
olehplavkov <108940216+olehplavkov@users.noreply.github.com>
2022-09-15 21:58:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-btc = float(input("How many $ costs 1 BTC today?"))
-money = float(input("How many $ do you have?"))
+btc = float(input("How many $ costs 1 BTC today?  "))
+money = float(input("How many $ do you have?  "))
 def crypt(btc, money):
     return money / btc
-    print("You can buy " + crypt + "BTC")
+    print("You can buy " + crypt + "BTC!")
 print("You can buy " + str(crypt(money, btc)) + " BTC")
